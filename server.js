d4797f9e6e7313fddf0319a153b753f03a7f7a79
No name <bluestardng@gmail.com>
2023-05-27 15:44:43 +0700

    #4 fix bug .gitignore push .env

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app')
-
-const PORT_NUMBER = 3005;
-
-const server = app.listen(PORT_NUMBER, () =>{
-    console.log(`WSV start on port ${PORT_NUMBER}`)
-})
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const express = require("express")
-const router = express.Router()
-
-router.use("/v1/api", require("./bootstrap/bootstrap.router") );
-
-module.exports = router;
